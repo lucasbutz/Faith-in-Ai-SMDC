bd76a191dd02dfe9429de6041895604ba459e078
Lucas BuchlI <lucasb26@uw.edu>
2025-03-21 20:42:58 -0700

    outputted sorted word frequency csv

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def searchReddit(r, query):
         posts_data.append({
         'title': post.title,
         'score': post.score,
-        'id': post.id,
+        'post_id': post.id,
         'url': post.url,
         'num_comments': post.num_comments,
         'created': post.created_utc,
@@ -62,20 +62,65 @@ if __name__ == "__main__":
     search5 = searchReddit(reddit, "talk with AI Muhammed")
     search6 = searchReddit(reddit, "AI Priest")
     
+    # Combine all search results
+    all_posts = search1 + search2 + search3 + search4 + search5 + search6
+    
+    for post in all_posts:
+        # make columns purpose clear
+        post['body_text'] = post.pop('body')
+    
+    # Remove duplicates based on post id
+    unique_posts = {}
+    for post in all_posts:
+        unique_posts[post['post_id']] = post
+    
+    # Convert back to list
+    unique_posts_list = list(unique_posts.values())
+    
+    print("Collected " + str(len(all_posts)) + " total posts")
+    print("After removing duplicates: " + str(len(unique_posts_list)) + " unique posts")
+    
     # Save to CSV
     print("Saving posts to CSV...")
     with open('reddit_outputs.csv', mode='w', newline='', encoding='utf-8') as f:
-        writer = csv.DictWriter(f, fieldnames=search1[0].keys())
-        writer.writeheader()
-        writer.writerows(search1)
-        writer.writerows(search2)
-        writer.writerows(search3)
-        writer.writerows(search4)
-        writer.writerows(search5)
-        writer.writerows(search6)
+        if unique_posts_list:
+            writer = csv.DictWriter(f, fieldnames=unique_posts_list[0].keys())
+            writer.writeheader()
+            writer.writerows(unique_posts_list)
+            print("CSV saved successfully")
+        else:
+            print("No posts found to save")
+       
+       
+    all_posts_text = ""
+    for post in unique_posts_list:
+        all_posts_text += post['title'] + " " + post['body_text'] + " "
         
-    print("CSV saved successfully!")
+    print("All posts combined into one text file")
+    with open('reddit_outputs.txt', 'w', encoding='utf-8') as f:
+        f.write(all_posts_text)
+        print("Text file saved successfully")
+    
+    word_freqencies = {}
+    for word in all_posts_text.split():
+        if word in word_freqencies:
+            word_freqencies[word] += 1
+        else:
+            word_freqencies[word] = 1
+        
+    # Sort by frequency of words, hishest freq at top, will need to take out stop words later        
+    sorted_x = sorted(word_freqencies.items(), key=lambda kv: kv[1])
+    sorted_x.reverse()
+    word_freqencies = dict(sorted_x)
+            
+    print("Word frequencies calculated")
+    with open('word_frequencies.csv', mode='w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(["Word", "Frequency"])
+        for word, frequency in word_freqencies.items():
+            writer.writerow([word, frequency])
+        print("Word frequencies saved successfully")
+    
 
-        
 
 
